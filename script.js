2e625500514ce055fc093c8476e8c4291c202ec2
Saurav <jhasaurav593@gmail.com>
2025-06-16 00:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+const table = document.querySelector("table");
+  let prices = 0;
+  const totalPrice = document.querySelectorAll(".price");
+  const tr = document.createElement("tr")
+  totalPrice.forEach((price) => {
+    prices += parseInt(price.textContent);
+    tr.innerHTML = `
+    <td class="price">${prices}</td>
+    `
+  });
+  table.appendChild(tr);
   
 };
 
